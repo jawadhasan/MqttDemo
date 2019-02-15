9f7990a1e2a3c52de7566de0143f20c1c33f7fb7
Shani <Shani@hds.eu>
2019-02-15 09:40:49 +0100

    consolePublisher added

---
+++ MqttSample/ConsolePublisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
